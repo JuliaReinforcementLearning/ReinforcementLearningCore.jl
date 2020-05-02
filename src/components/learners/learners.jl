6e725b22bb16e3fb6636e04f2bfeed5bdccdbc94
Jun Tian <tianjun.cpp@gmail.com>
2020-05-02 14:53:14 +0800

    clean up learners

--- src/components/learners/learners.jl
+++ src/components/learners/learners.jl
@@ -1,1 +1,20 @@
-include("double_learner.jl")
+"""
+    (learner::AbstractLearner)(obs)
+
+A learner is usually used to estimate state values, state-action values or distributional values based on experiences.
+"""
+abstract type AbstractLearner end
+
+function (learner::AbstractLearner)(obs) end
+
+"""
+    update!(learner::AbstractLearner, experience)
+
+Typical `experience` is [`AbstractTrajectory`](@ref).
+"""
+function RLBase.update!(learner::AbstractLearner, experience) end
+
+"""
+    get_priority(p::AbstractLearner, experience)
+"""
+function RLBase.get_priority(p::AbstractLearner, experience) end
