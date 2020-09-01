af9047bac9b2057e81b08fd9e6d152967a0446ad
Jun Tian <tianjun.cpp@gmail.com>
2020-09-01 12:14:33 +0800

    fix bug in TabularLearner

--- src/components/learners/tabular_learner.jl
+++ src/components/learners/tabular_learner.jl
@@ -10,8 +10,8 @@ struct TabularLearner{S,T} <: AbstractPolicy
 end
 
 TabularLearner() = TabularLearner{Int,Float32}()
-TabularLearner{S}() = TabularLearner{S,Float32}()
-TabularLearner{S,T}() = TabularLearner(Dict{S,Vector{T}}())
+TabularLearner{S}() where S = TabularLearner{S,Float32}()
+TabularLearner{S,T}() where {S,T} = TabularLearner(Dict{S,Vector{T}}())
 
 function (p::TabularLearner)(env::AbstractEnv)
     s = get_state(env)
