a142edf883248a2cfa9543fb6f49f3eb0a64af63
Roman Bange <roman.bange@posteo.de>
2020-09-26 01:21:30 +0200

    add GPU differentiable logpdf for normal distributions

--- test/extensions.jl
+++ test/extensions.jl
@@ -14,3 +14,15 @@
     clip_by_global_norm!(gs, ps, 4.0f0)
     @test isapprox(gs[:x], [0.0 0.0 0.0; 0.0 0.0 0.0])
 end
+
+
+@testset "Distributions" begin
+    @test isapprox(logpdf(Normal(), 2), normlogpdf(0, 1, 2))
+    @test isapprox(logpdf.([Normal(), Normal()], [2, 10]), normlogpdf([0, 0], [1, 1], [2, 10]))
+
+    # Test numeric stability for 0 sigma
+    @test isnan(normlogpdf(0, 0, 2, ϵ=0))
+    @test !isnan(normlogpdf(0, 0, 2))
+
+    # GPU differentiability not testable
+end
